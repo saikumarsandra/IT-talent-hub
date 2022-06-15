8900a52f6892b5c7818d95d312e97acfa8c6bb98
Sandra Sai Kumar <SaikumarSandra.Kumar@cognizant.com>
2022-06-15 10:08:03 +0100

    practice

--- myReactapp/practiceapp/src/App.js
+++ myReactapp/practiceapp/src/App.js
@@ -3,12 +3,9 @@
 function App() {
   return (
     <div className="App">
-     <h1>my react app</h1>
+      <h1 className="text-info">Hello World</h1>
     </div>
   );
 }
 
 export default App;
-
-
-
--- myReactapp/practiceapp/src/index.js
+++ myReactapp/practiceapp/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,5 +9,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
 
